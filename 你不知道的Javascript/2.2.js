894f139cdbd36f50912b19487cb9f5855ef66695
Kirawk <1184355944@qq.com>
2017-05-10 00:13:25 +0800

    with修改作用域

--- 你不知道的Javascript/2.2.js
+++ 你不知道的Javascript/2.2.js
@@ -17,3 +17,30 @@ function foo(str){
 	console.log(str);//a is not defined 
 }
 foo("var a=2");
+
+/*2.2.2
+*with
+*/
+function foo(obj){
+	with(obj){
+		a=2;
+
+	}
+}
+var o1={
+	a:3;
+};
+var o2={
+
+	b:4
+};
+foo(o1);
+console.log(o1.a);//2
+foo(o2);
+console.log(o2.a);//undefind
+console.log(a);//2
+
+
+
+
+
