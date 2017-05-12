70de590c676526013f42bae69a607ff6e345ae09
Kirawk <1184355944@qq.com>
2017-05-12 23:15:08 +0800

    立即执行函数

--- 你不知道的Javascript/3.0.js
+++ 你不知道的Javascript/3.0.js
@@ -21,3 +21,79 @@ function doSomething(a){
 	cosole.log(b*3);
 }
 doSomething(2);//
+
+//规避冲突
+function foo(){
+
+	function bar(a){
+		i=3;
+		console.log(a+i);
+	}
+
+	for(var i=0;i<10;i++){
+		bar(i*2);
+	}
+}
+foo();
+
+/*
+*同名标识符造成的冲突
+*/
+function foo(){
+	function bar(a){
+
+		i=3;
+		console.log(a+i);
+	}
+    for(var i=0;i<10;i++){
+    	bar(i*2);//因为i值被覆盖为3，造成死循环
+    }
+}
+foo();
+
+/*
+*3.3
+*/
+var a=2;
+function foo(){
+	var a=3;
+	console.log(a);//3
+}
+foo();
+console.log(a);//2
+//改进
+var a=2;
+(function foo(){
+	var a=3;
+	console.log(a);
+})();
+console.log(a);
+
+/*
+*3.3.2立即执行函数IIFE
+*/
+var a=2;
+(function IIFE(){
+	var a=3;
+	console.log(a);//3
+})();
+console.log(a);//2
+
+//立即执行函数进阶用法
+var a=2;
+(function IIFE(global){
+	var a=3;
+	console.log(a);//3
+	console.log(global.a);//2
+})(window);
+console.log(a);//2
+
+//在UMD项目中广泛使用
+var a=2;
+(function IIFE(def){
+	def(window);
+})(function def(global){
+	var a=3;
+	console.log(a);//3
+	console.log(global.a);//2
+});
