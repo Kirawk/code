06fc5d8b2b2413b92ba7eee6d7ca2e631580683b
Kirawk <1184355944@qq.com>
2017-05-19 23:11:09 +0800

    创建count对象来记录调用次数

--- 你不知道的Javascript/part2-1.0.js
+++ 你不知道的Javascript/part2-1.0.js
@@ -31,3 +31,18 @@ for(i=0;i<10;i++){
 	}
 }
 console.log(foo.count);
+
+function foo(num){
+	console.log("foo"+num);
+	data.count++;
+}
+var data={
+	count:0;
+}
+var i;
+for(var i=0;i<10;i++){
+	if(i>5){
+		foo(i);
+	}
+}
+console.log(data.count);
