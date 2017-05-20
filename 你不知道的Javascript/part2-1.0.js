ab302c984435250314213066cb6cdd97760dab2b
Kirawk <1184355944@qq.com>
2017-05-20 23:02:46 +0800

    使用foo标识符代替this

--- 你不知道的Javascript/part2-1.0.js
+++ 你不知道的Javascript/part2-1.0.js
@@ -46,3 +46,25 @@ for(var i=0;i<10;i++){
 	}
 }
 console.log(data.count);
+
+function foo(num){
+	console.log("foo"+num);
+	foo.count++;
+
+}
+foo.count=0;
+var i=0;
+for(i=0;i<10;i++){
+	if(i>5){
+		foo(i);
+	}
+}
+console.log(foo.count);
+
+
+
+
+
+
+
+
