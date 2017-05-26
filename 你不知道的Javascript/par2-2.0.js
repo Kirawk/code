c1ba2f3eca41c5561c616cb75cb714db323c5522
Kirawk <1184355944@qq.com>
2017-05-26 22:50:22 +0800

    this默认绑定规则

--- 你不知道的Javascript/par2-2.0.js
+++ 你不知道的Javascript/par2-2.0.js
@@ -19,3 +19,17 @@ function foo(){
 
 }
 baz();//baz调用的位置
+
+/*默认绑定*/
+function foo(){
+	console.log(this.a);
+}
+var a=2;
+foo();//2
+//使用严格模式，默认不会绑定到全局对象上
+functio foo(){
+	"user strict";
+	console.log(this.a);
+}
+var a=2;
+foo();//TypeEror:this is undefined
