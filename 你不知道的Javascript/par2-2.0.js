a89ad179dfbe4d976f3226b5107f2a6a46f09bac
Kirawk <1184355944@qq.com>
2017-05-27 23:03:43 +0800

    隐式绑定

--- 你不知道的Javascript/par2-2.0.js
+++ 你不知道的Javascript/par2-2.0.js
@@ -33,3 +33,27 @@ functio foo(){
 }
 var a=2;
 foo();//TypeEror:this is undefined
+
+/*隐士绑定*/
+function foo(){
+	console.log(this.a);
+}
+var obj={
+	a:2;
+	foo:foo;
+}
+
+obj.foo();//2
+//隐士丢失
+function foo(){
+	console.log(this.a);
+}
+var obj2={
+	a:42;
+	foo:foo;
+}
+var obj1={
+	a:2;
+	obj2:obj2;
+}
+obj1.obj2.foo();//42
