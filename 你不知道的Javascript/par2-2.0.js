e3dba8e1e349300669bb16238ba3cb9d367fe969
Kirawk <1184355944@qq.com>
2017-06-07 23:03:21 +0800

    显示绑定比隐式绑定优先级高

--- 你不知道的Javascript/par2-2.0.js
+++ 你不知道的Javascript/par2-2.0.js
@@ -176,6 +176,25 @@ function foo(a){
 var bar=new foo(2);
 cossole.log(bar.a);//2
 
+/*优先级*/
+function foo(){
+	console.log(this.a);
+}
+var obj1{
+	a:2
+	foo:foo
+};
+var obj2{
+	a:3
+	foo:foo
+};
+obj1.foo();//2
+obj2.foo();//3
+
+obj1.foo.call(obj2);//3
+obj2.foo.call(obj1);//2
+
+
 
 
 
