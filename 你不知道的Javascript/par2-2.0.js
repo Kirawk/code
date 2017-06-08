9a4fd7115481d2b6b7f1c8f4f711c7868d340acc
Kirawk <1184355944@qq.com>
2017-06-08 23:23:14 +0800

    new 绑定与隐式绑定的优先级

--- 你不知道的Javascript/par2-2.0.js
+++ 你不知道的Javascript/par2-2.0.js
@@ -194,6 +194,24 @@ obj2.foo();//3
 obj1.foo.call(obj2);//3
 obj2.foo.call(obj1);//2
 
+function foo(something){
+	this.something=somenthing;
+}
+var obj1={
+	foo:foo
+}
+
+var obj2={}
+
+obj1.foo(2);
+console.log(obj1.a);//2
+obj1.foo.call(obj2,3);
+console.log(obj2.a);//3
+
+var bar=new obj1.foo(4);
+console.log(obj1.a);//2
+console.log(bar.a);//4
+
 
 
 
