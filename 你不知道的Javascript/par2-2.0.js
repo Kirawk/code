a6900386dbc770736a16370f24bd9191c2aaa744
Kirawk <1184355944@qq.com>
2017-06-09 22:41:03 +0800

    new绑定与硬绑定比较

--- 你不知道的Javascript/par2-2.0.js
+++ 你不知道的Javascript/par2-2.0.js
@@ -212,6 +212,23 @@ var bar=new obj1.foo(4);
 console.log(obj1.a);//2
 console.log(bar.a);//4
 
+/*new绑定*/
+function foo(something){
+	this.a=something;
+}
+var obj1={
+
+};
+var bar=foo.bind(obj1);
+bar(2);
+console.log(obj1.a);//2
+
+var baz=new bar(3);
+console.log(obj1.a);//2
+console.log(baz.a);//3??????
+
+
+
 
 
 
