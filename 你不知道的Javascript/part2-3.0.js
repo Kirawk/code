4c4895cc111a36e438195aaf400df6efc0a9c7a6
Kirawk <1184355944@qq.com>
2017-06-16 22:53:47 +0800

    Javascript对象开始

---
+++ 你不知道的Javascript/part2-3.0.js
@@ -0,0 +1,8 @@
+/*对象*/
+var string ="I am string";
+typeof string;//"string"
+string instanceof String;//false;
+
+var string=new String("I am string");
+typeof string;//"Object"
+string instanceof String;//true
