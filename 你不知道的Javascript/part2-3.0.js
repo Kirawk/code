d692147c67b01df9e711affe2dc558b3e4672db4
Kirawk <1184355944@qq.com>
2017-06-18 22:57:33 +0800

    访问对象属性

--- 你不知道的Javascript/part2-3.0.js
+++ 你不知道的Javascript/part2-3.0.js
@@ -10,3 +10,22 @@ string instanceof String;//true
 var string="I am string";
 console.log(string.length);
 console.log(string.charAt(3));//
+
+/*访问对象属性*/
+var obj={
+    a:2
+};
+console.log(obj.a);//2
+console.log(obj["a"]);//2
+
+var obj={
+	a:2
+};
+var idx;
+function something(wantA){
+  if(wantA){
+	idx="a";
+}
+}
+something(true);
+console.log(obj[idx]);//2
