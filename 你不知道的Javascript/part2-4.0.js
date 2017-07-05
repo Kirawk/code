07deb85b5d355bbe5a23260f899ca408d1e896da
Kirawk <1184355944@qq.com>
2017-07-05 23:25:09 +0800

    寄生式继承

--- 你不知道的Javascript/part2-4.0.js
+++ 你不知道的Javascript/part2-4.0.js
@@ -37,4 +37,32 @@ mixin({
 		Vehicle.drive.call(this);
 		console.log("Rolling on all"+this.wheels+"wheels!");
 	}
-})
+});
+
+/*寄生继承*/
+function Vehicle (){
+    this.engines = 1;
+}
+Vehicle.prototype.ignition = function () {
+	console.log("Turing on my engine");
+};
+Vehicle.prototype.drive = function () {
+	this.ignition();
+	console.log("Steering and moving forward");
+}
+//寄生类car
+function Car () {
+	var car = new Vehicle();
+	car.wheels = 4;
+	var vehDrive = car.drive;
+	car.drive = function () {
+		vehDrive.call(this);
+		console.log( "Rolling on all" + this.wheels + "wheels!");
+		
+	};
+	return car;
+}
+var myCar =new Car();
+myCar.drive();
+
+
