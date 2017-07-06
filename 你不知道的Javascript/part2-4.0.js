c3b5c65e960835b116620f6b591334abc6e2b3c3
Kirawk <1184355944@qq.com>
2017-07-06 22:53:35 +0800

    隐式混入

--- 你不知道的Javascript/part2-4.0.js
+++ 你不知道的Javascript/part2-4.0.js
@@ -65,4 +65,25 @@ function Car () {
 var myCar =new Car();
 myCar.drive();
 
+/*隐式混合*/
+var Something={
+	cool:function () {
+		this.greeting = "Hello World";
+		this.count = this.count ? this.count +1 : 1;
+	}
+};
+Something.cool();
+Something.greeting;
+Something.count;
+
+var Another ={
+	cool: function() {
+		Something.cool.call(this);
+	}
+};
+Another.cool();
+Another.greeting;
+Another.count;
+
+
 
