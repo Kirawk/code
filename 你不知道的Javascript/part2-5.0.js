1c386a3c7216c08357720abffd522f6cc60421d0
Kirawk <1184355944@qq.com>
2017-07-08 22:23:33 +0800

    隐式屏蔽

--- 你不知道的Javascript/part2-5.0.js
+++ 你不知道的Javascript/part2-5.0.js
@@ -8,4 +8,17 @@ for(var k in myObejct) {
 	console.log("found:"+k);
 }
 
+/*属性设置与屏蔽*/
+var anotherObject={
+	a:2
+};
+var myObejct=Object.create(anotherObject);
+
+anotherObject.a;//2
+myObejct.a;//2
+anotherObject.hasOwnProperty("a");//true
+myObejct.hasOwnProperty("a");//true
+myObejct.a++;//隐式屏蔽；
+myObejct.a;//3
+anotherObject.a;//2
 
