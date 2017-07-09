8d2ceaee4f1e2d7ac7a880b7869eab6e40fd076f
Kirawk <1184355944@qq.com>
2017-07-09 22:54:19 +0800

    Javascript继承剖解

--- 你不知道的Javascript/part2-5.0.js
+++ 你不知道的Javascript/part2-5.0.js
@@ -22,3 +22,21 @@ myObejct.a++;//隐式屏蔽；
 myObejct.a;//3
 anotherObject.a;//2
 
+/*构造函数还是调用*/
+function NothingSpecial(){
+	console.log("Don't mind me");
+}
+var a = new NothingSpecial();
+console.log(a);
+
+/*伪类*/
+function Foo(name){
+	this.name=name;
+}
+Foo.prototype.myName = function() {
+	return this.name;
+};
+var a = new Foo("a");
+var b = new Foo("b");
+a.myName();
+b.myName();  //b
