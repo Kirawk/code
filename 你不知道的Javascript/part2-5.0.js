10021d73be4353dbfdb78518cf81fbd81cdb6830
Kirawk <1184355944@qq.com>
2017-07-10 22:48:30 +0800

    原型继承

--- 你不知道的Javascript/part2-5.0.js
+++ 你不知道的Javascript/part2-5.0.js
@@ -40,3 +40,24 @@ var a = new Foo("a");
 var b = new Foo("b");
 a.myName();
 b.myName();  //b
+
+/*原型继承*/
+function Foo(name){
+	this.name=name;
+}
+Foo.prototype.myName = function () {
+	return this.name;
+};
+function Bar (name,label) {
+	Foo.call(this,name);
+	this.label = label;
+}
+Bar.prototype = Object.create(Foo.prototype);
+Bar.prototype.myLabel = function () {
+	return this.label;
+};
+var a =new Bar("a","obj a");
+a.myName();//a
+a.myLabel();//obj a
+
+
