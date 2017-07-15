94f04973649d04be01bdc762617dfc7c03bba08f
Kirawk <1184355944@qq.com>
2017-07-15 23:52:09 +0800

    跟踪对象内部

--- 你不知道的Javascript/part2-6.0.js
+++ 你不知道的Javascript/part2-6.0.js
@@ -24,3 +24,12 @@ Foo.prototype.constructor = function Gotcha();
 a1.constructor;
 a1.constructor.name;
 a1;//foo{}
+
+var Foo =　{};
+var a1 = Object.create(Foo);
+a1;
+Object.defineProperty(Foo,"constructor",{
+	enumerable: false,
+	value: function Gotcha(){}
+});
+a1;
