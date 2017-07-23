5d3ab0e4fef1087ed72384cb58ff74bbc95a2efc
Kirawk <1184355944@qq.com>
2017-07-23 22:32:43 +0800

    类关联与对象关联比较

--- 你不知道的Javascript/part2-6.0.js
+++ 你不知道的Javascript/part2-6.0.js
@@ -146,3 +146,219 @@ class  Widget{
  		console.log("Button"+this.label+"clicked!");
  	}
  }
+ $(document).ready(function(){
+ 	var $body = $(document.body);
+ 	var btn1 = new Button(125,30,"hello");
+ 	var btn2 = new Button(150,40,"world");
+
+ 	btn1.render($body);
+ 	btn2.render($body);
+ });
+
+ /*对象关联委托*/
+  var Widget={
+  	init: function(width,height){
+  		this.width = width || 50;
+  		this.height = height || 50;
+  		this.$elem = null;
+  	},
+  	insert: function ($where){
+  		if(this.$elem){
+  			this.$elem.css({
+  				width:this.width + "px",
+  				height:this.height + "px"
+  			}).appendTo($where);
+  		}
+  	}
+  };
+
+  var Button Object.create(Widget);
+  Button.setup = function(width,height,label){
+  	//委托调用
+  	this.init(width,height);
+  	this.label = label || "Default";
+
+  	this.$elem =$("<button>").text(this.label);
+  };
+  Button.build = function ($where) {
+  	//委托调用
+  	 this.insert($where);
+  	 this.$elem.click(this.onClick.bind(this));
+  };
+  Button.onClick = function (evt) {
+  	console.log("button"+this.label+"clicked!");
+  };
+  $(document).ready(function(){
+  	var $body = $(document.body);
+  	var btn1 = Object.create(Button);
+  	btn1.setup(125,30,"hello");
+
+  	var btn2 =Object.create(Button);
+  	btn2.setup(150,40,"world");
+
+  	btn1.build($body);
+  	btn2.build($body);
+  })
+
+  /*登录的场景设计*/
+  /*类模式*/
+  function Controller(){
+  	this.errors = [];
+  }
+
+  Controller.prototype.showDialog = function (title,msg) {
+  	//显示标题与信息
+  }
+
+  Controller.prototype.success = function (msg){
+  	this.showDialog("Success",msg);
+  }
+
+  Controller.prototype.failure = function (err) {
+  	this.errors.push(err);
+  	this.showDialog("Error",err);
+  }
+
+  //子类
+  function LoginController(){
+  	Controller.call(this);
+  }
+
+  //把子类关联到父类
+
+  LoginController.prototype = Object.create(Controller.prototype);
+  LoginController.prototype.getUser = function () {
+  	return document.getElementById("login_username").value;
+  };
+  LoginController.prototype.getPassword= function () {
+  	return document.getElementById("login_password").value;
+  };
+  LoginController.prototype.validateEntry = function (user,pw) {
+  	user = user || this.getUser();
+  	pw = pw || this.getPassword();
+
+  	if(!(user&& pw)){
+  		return this.failure(
+  			"please enter a username and password "
+  			);
+  	}
+  	else if( pw.length < 5) {
+  		return this.failure(
+  			"password must be 5+ characters!"
+  			);
+  	}
+
+  	return true;
+  };
+
+  //重写基础的failure();
+  LoginController.prototype.failure = function (err) {
+  	//super调用
+  	Controller.prototype.failure.call(this,"login invalid"+err);
+  };
+
+  //子类
+  function AuthController(login) {
+  	Controller.call(this);
+  	this.login = login;
+  }
+  //把子类关联到父类
+  AuthController.prototype = Object.create(Controller.prototype);
+  AuthController.prototype.server = function (url,data) {
+  	return $.ajax({
+  		url: url,
+  		data: data
+  	});
+  };
+
+  AuthController.prototype.checkAuth = function () {
+  	var user = this.login.getUser();
+  	var pw = this.login.getPassword();
+
+  	if (this.login.validateEntry(user,pw)){
+  		this.server("/check-auth",{
+  			user: user,
+  			pw : pw
+  		})
+  		.then( this.success.bind(this))
+  		.fail( this.failure.bind(this));
+
+  	}
+  };
+
+  //重写基础success()
+  AuthController.prototype.success = function () {
+  	//super调用
+  	Controller.prototype.success.call(this,"Authenitcated!");
+  }
+
+  //重写基础的failure()
+  AuthController.prototype.failure = function (err){
+  	//super调用
+  	Controller.prototype.failure.call(this,"Auth Failed:"+err);
+  };
+
+  var auth = new AuthController(new LoginController());;
+  auth.checkAuth();
+
+  //使用对象关联实现上述代码
+   var LoginController = {
+   	errors: [],
+   	getUser: function (){
+   		return document.getElementById("login_username").value;
+   	},
+   	getPassword: function () {
+   		return document.getElementById("login_password").value;
+   	},
+   	validateEntry: function (user,pw) {
+   		user = user || this.getUser();
+   		pw =pw||this.getPassword();
+   		if(!(user&&pw)){
+   			return this.failure("please enter a name and password");
+   		}else if(pw.length<5){
+   			return this.failure("password must be 5+ characters!");
+   		}
+   		return true;
+   	},
+   	showDialog: function(title,msg){
+   		//提示语
+   	},
+   	failure: function (err){
+   		this.errors.push(err);
+   		this.showDialog("Error","login invalid"+err);
+   	}
+   };
+
+   //让AuthController委托LoginController
+   var AuthController = Object.create(LoginController);
+
+   AuthController.errors = [];
+   AuthController.checkAuth = function () {
+   	var user = this.getUser();
+   	var pw = this.getPassword();
+
+   	if(this.validateEntry(user,pw)){
+   		this.server("/check-auth",{
+   			user: user,
+   			pw: pw
+   		})
+   		.then(this.accepted.bind(this));
+   		.fail(this.rejected.bind(this));
+   	}
+   };
+   AuthController.server = function (url,data) {
+   	return $ajax({
+   		url:url,
+   		data:data
+   	});
+   };
+   AuthController.accepted = function () {
+   	this.showDialog("Success","Authenitcated");
+   };
+   AuthController.rejected = function (err) {
+   	this.failure("Auth Failed"+ err);
+   };
+   AuthController.checkAuth();
+
+
+
