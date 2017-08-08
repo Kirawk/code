19927fad0f2be18faa526d45a0473709daf6b6b3
Kirawk <1184355944@qq.com>
2017-08-08 23:10:28 +0800

    命令模式与高阶函数

--- Javascript设计模式/3.0.js
+++ Javascript设计模式/3.0.js
@@ -157,3 +157,102 @@ var extent = new Extent();
 extend.call();
 extend.call();
 extend.call();
+/**用闭包实现命令模式****/
+// <button id="execute">点击我执行命令</button>
+//  <button id="undo">点击我执行命令</button> 
+
+//对象版命名模式
+var Tv ={
+    open:function(){
+        console.log("打开电视机");
+    },
+    close:function(){
+        console.loh("关闭电视");
+    }
+};
+var OpenTvCommand = function(receiver){
+    this.receiver=receiver;
+};
+OpenTvCommand.prototype.execute = function(){
+    this.receiver.open();
+};
+OpenTvCommand.prototype.undo = function(){
+    this.receiver.close()
+};
+var setCommand = function(command){
+    document.getElementById('execute').onclick = function(){
+        command.execute();
+    }
+    document.getElementById("undo").onclick=function(){
+        command.undo();
+    }
+};
+setCommand(new OpenTvCommand(Tv));
+
+//闭包版命令模式
+var Tv ={
+    open:function(){
+        console.log("打开电视机");
+    },
+    close:function(){
+        console.loh("关闭电视");
+    }
+};
+
+var createCommand=function(receiver){
+    var execute=function(){
+        return receiver.open();
+    }
+    var undo=function(){
+        return receiver.close();
+    }
+    return {
+        execute:execute,
+        undo:undo
+    }
+};
+var setCommand=function(command){
+    document.getElementById("execute").onclick=function(){
+        command.execute();
+    }
+    document.getElementById("undo").onclick=function(){
+        command.undo();
+    }
+};
+setCommand(createCommand(Tv));
+
+/*3.2.1 高阶函数*/
+//函数作为参数回调
+
+//回调函数
+/*创建100div，并将它们隐藏*/
+var appendDiv = function(){
+    for(var i=0;i<10;i++){
+        var div = document.createElement('div');
+        div.innerHTML=i;
+        document.body.appendChild(div);
+        div.style.display='none';
+    }
+};
+appendDiv();
+
+var appendDiv = function(callback){
+    for(var i=0;i<10;i++){
+        var div= document.createElement('div');
+        div.innerHTML=i;
+        document.body.appendChild(div);
+        if(typeof callback==='function'){
+            callback(div);
+        }
+    }
+};
+appendDiv(function(node){
+    node.style.display='none';
+});
+/*Array,prototype.sort*/
+[1,4,3].sort(function(a,b){
+    return a-b;
+});
+[1,4,3].sort(function(a,b){
+    return b-a;
+})
