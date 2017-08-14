824998e3926f31937714f4fd4d18c8cb04176b7b
Kirawk <1184355944@qq.com>
2017-08-14 23:10:48 +0800

    分时函数

--- Javascript设计模式/3.0.js
+++ Javascript设计模式/3.0.js
@@ -467,3 +467,18 @@ window.onresize = throttle(function(){
     console.log(1);
 },500);
 
+//分时函数
+var ary = [];
+for(var i=1;i<=1000;i++){
+    ary.push(i);
+};
+var renderFriendList = function(data){
+
+    for(var i =0,l =data.length;i<l;i++){
+        var div = document.createElement('div');
+        div.innerHTML = i;
+        document.body.appendChild(div);
+    }
+};
+renderFriendList(ary);
+
