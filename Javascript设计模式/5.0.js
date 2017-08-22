13215365af4422b50ec80c346625a81759c9741f
Kirawk <1184355944@qq.com>
2017-08-22 23:09:50 +0800

    面向对象方法使用策略模式

--- Javascript设计模式/5.0.js
+++ Javascript设计模式/5.0.js
@@ -39,3 +39,57 @@ var calculateBonus =function(performanceLevel,salary){
 };
 calculateBonus('A',10000);
 
+//重写上述代码
+var performanceS = function(){};
+performanceS.prototype.calculate = function(salary){
+    return salary*4;
+};
+var performanceA = function(){};
+performanceA.prototype.calculate = function(salary){
+    return salary*3;
+};
+ var performanceB = function(){};
+ performanceB.prototype.calculate = function(salary){
+     return salary*2;
+ };
+//定义Bonus奖金类
+ var Bonus = function(){
+     this.salary=null;    //原始工资
+     this.strategy = null;//绩效等级对应的策略对象
+ };
+ Bonus.prototype.setSalary = function(salary){
+     this.salary=salary;   //设置员工的原始工资
+ };
+ Bonus.prototype.setStrategy = function(strategy){
+     this.strategy = strategy;
+ };
+ Bonus.prototype.getBonus = function(){
+     return this.strategy.calculate(this.salary);
+ };
+
+var bonus = new Bonus();
+bonus.setSalary( 10000 );
+bonus.setStrategy( new performanceS() ); // 设置策略对象
+console.log( bonus.getBonus() ); // 输出：40000
+bonus.setStrategy( new performanceA() ); // 设置策略对象
+console.log( bonus.getBonus() ); // 输出：30000 
+
+ //Javascript版本的策略模式
+var strategies = {
+
+    "S":function(salary){
+        return salary*4;
+    },
+    "A":function(salary){
+        return salary*3;
+    },
+    "B":function(salary){
+        return salary*2;
+    }
+};
+var calculateBonus = function(level,salary){
+    return strategies[level](salary);
+};
+console.log(calculateBonus('S',20000));
+console.log(calculateBonus('A',10000));
+
