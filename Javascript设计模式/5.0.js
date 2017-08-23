395117857aef787bc03ea4ef41e50f6cbb1ab034
Kirawk <1184355944@qq.com>
2017-08-23 23:13:29 +0800

    策略模式在动画中的应用

--- Javascript设计模式/5.0.js
+++ Javascript设计模式/5.0.js
@@ -93,3 +93,26 @@ var calculateBonus = function(level,salary){
 console.log(calculateBonus('S',20000));
 console.log(calculateBonus('A',10000));
 
+//让小球运动起来
+var tween = {
+    linear: function(t, b, c, d) {
+        return c * t / d + b;
+    },
+    easeIn: function(t, b, c, d) {
+        return c * (t /= d) * t + b;
+    },
+    strongEaseIn: function(t, b, c, d) {
+        return c * (t /= d) * t * t * t * t + b;
+    },
+    strongEaseOut: function(t, b, c, d) {
+        return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+    },
+    sineaseIn: function(t, b, c, d) {
+        return c * (t /= d) * t * t + b;
+    },
+    sineaseOut: function(t, b, c, d) {
+        return c * ((t = t / d - 1) * t * t + 1) + b;
+    }
+};
+
+
