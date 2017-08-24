81a697a1bee7cdc1afd86eb4727429206b478d34
Kirawk <1184355944@qq.com>
2017-08-24 23:03:39 +0800

    实现动画的原理

--- Javascript设计模式/5.0.js
+++ Javascript设计模式/5.0.js
@@ -115,4 +115,49 @@ var tween = {
     }
 };
 
+//定义Animate类
+var Animate = function(dom){
+    this.dom = dom;
+    this.startTime = 0;//动画开始时间
+    this.startPos = 0;//动画开始时，dom节点的位置，即dom的初始位置
+    this.endPos = 0; //动画介绍时,dom节点的位置，即dom的目标位置
+    this.propertyName = null; //dom节点需要被改变的css属性名
+    this.easing = null; //缓动算法
+    this.duration = null; //动画持续时间
+}
+Animate.prototype.start = function(propertyName,endPos,duration,easing){
+    this.startTime =+new Date; //动画启动时间
+    this.startPos = this.dom.getBoundingClientRect()[propertyName];
+    this.propertyName = propertyName;
+    this.endPos = endPos;//dom 节点目标位置
+    this.duration = duration; //动画持续事件
+    this.easing = tween[easing];//缓动算法
+
+    var self = this;
+    var timeId =setInterval(function(){ //启动定时器，开始执行动画
+        if(self.step()===false){        //如果动画已结束，则清除定时器
+            clearInterval(timeId); 
+        }
+    },19);
+};
+//小球运动的每一帧要做的事
+Animate.prototype.step = function(){
+    var t = +new Date;
+    if(t>=this.startTime+this.duration){
+        this.update(this.endPos);
+        return false; 
+    }
+    var pos = this.easing(t-this.startTime,this.startPos,this.endPos-this.startPos,this.duration);//pos为小球当前位置
+    this.update(pos);//更新小球的css属性值
+};
+//更新方法
+Animate.prototype.update = function(pos){
+    this.dom.style[this.propertyName]=pos+'px';
+};
+//测试
+/*<div style="position:absolute;background:blue" id="div">我是 div</div> */
+var div = document.getElementById( 'div' );
+var animate = new Animate( div );
+animate.start( 'left', 500, 9000, 'strongEaseOut' ); 
+
 
