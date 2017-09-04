77f35cd4a2dd3ae175f14815ff148f92e87f80f1
Kirawk <1184355944@qq.com>
2017-09-04 22:38:16 +0800

    缓存代理的应用

--- Javascript设计模式/6.0.js
+++ Javascript设计模式/6.0.js
@@ -140,3 +140,67 @@ var proxySynchronousFile = (function(){
  }, 2000 );
  }
 })(); 
+
+/*********6.8缓存代理*******/
+//计算乘积
+var mult = function(){
+    console.log("开始计算乘积");
+    var a=1;
+    for(var i =0,l = arguments.length;i<l;i++){
+        a = a*arguments[i];
+    }
+    return a;
+};
+mult(2,3);//6
+mult(2,3,4);//24
+//加入缓存代理函数
+var proxyMult = (function(){
+    var cache = {};
+    return function(){
+        var args = Array.prototype.join.call(arguments,',');
+        if(args in cache){
+            return cache[args];
+        }
+        return cache[args] = mult.apply(this,arguments);
+    }
+})();
+proxyMult(1,2,3,4);
+proxyMult(1,2,3,4);
+
+//用于ajax异步请求数据：网站中的分页
+
+/*6.9高阶函数动态创建代理*/
+//计算乘积
+var mult = function(){
+    var a = 1;
+    for(var i=0,l=arguments.length;i<l;i++){
+        a=a*arguments[i];
+    }
+    return a;
+};
+//计算加和
+var plus = function(){
+    var a = 0;
+    for(var i=0,l=arguments.length;i<l;i++){
+        a = a+arguments[i];
+    }
+    return a;
+};
+/****************创建缓存代理工厂********************/
+var createProxyFactory = function(fn){
+    var cache = {};
+    return function(){
+        var args =Array.prototype.join.call(arguments,',');
+        if(args in cache){
+            return cache[args];
+        }
+        return cache[args] = fn.apply(this,arguments);
+    }
+};
+var proxyMult = createProxyFactory(mult);
+proxyPlus =createProxyFactory(plus);
+console.log(proxyMult(1,2,3,4));
+console.log(proxyPlus(1,2,3,4));
+
+/**6.10 其他代理模式***/
+
