88a66917c9d7483a41e62ca135fa1f1039a3a711
Kirawk <1184355944@qq.com>
2017-09-09 23:02:24 +0800

    终止迭代器

--- Javascript设计模式/7.0.js
+++ Javascript设计模式/7.0.js
@@ -93,3 +93,17 @@ var reverseEach = function(ary, callback) {
 reverseEach([0, 1, 2], function(i, n) {
     console.log(n);
 });
+/*中止迭代器*/
+var each = function(ary,callback){
+    for(var i=0,l=ary.length;i<l;i++){
+        if(callback[i,ary[i]]===false){
+            break;
+        }
+    }
+};
+each([1,2,3,4,5],function(i,n){
+    if(n>3){
+        return false;
+    }
+    console.log(n);
+});
