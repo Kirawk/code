1743e76e29108f433bd7c5440ab3ef46a63b982d
Kirawk <1184355944@qq.com>
2017-09-12 23:19:23 +0800

    订阅模式

---
+++ Javascript设计模式/8.0.js
@@ -0,0 +1,11 @@
+/*发布订阅模式*/
+var salesOffices = {};//定义售楼
+salesOffices.clientList = [];//缓存列表，存放订阅者的回调函数
+salesOffices.listen = function(fn){
+    this.clientList.push(fn);//订阅消息添加进缓存列表
+};
+salesOffices.trigger = function(){
+    for(var i=0,fn;fn=this.clientList[i++];){
+        fn.apply(this,arguments);
+    }
+};
