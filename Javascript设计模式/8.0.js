4160361cae31a59b6fa4b8a50e589e8215865ed2
Kirawk <1184355944@qq.com>
2017-09-14 23:13:49 +0800

    改写代码

--- Javascript设计模式/8.0.js
+++ Javascript设计模式/8.0.js
@@ -19,3 +19,35 @@ salesOffices.listen(function(price,squareMeter){
 });
 salesOffices.trigger(200000,88);
 salesOffices.trigger(300000,100);
+
+//改写上述例子
+var salesOffices = {};
+salesOffices.clientList = {};
+salesOffices.listen = function(key, fn) {
+    if ((!this.clientList[key]) ) {
+        this.clientList[key] = [];
+    }
+    this.clientList[key].push(fn);
+};
+salesOffices.trigger = function() {
+    var key = Array.prototype.shift.call(arguments)
+      , fns = this.clientList[key];
+    if (!fns || fns.length === 0) {
+        return false;
+    }
+    for (var i = 0, fn; fn = fns[i++]; ) {
+        fn.apply(this, arguments);
+    }
+};
+salesOffices.listen('squareMeter88', function(price) {
+    // 小明订阅 88 平方米房子的消息
+    console.log('价格= ' + price);
+    // 输出： 2000000
+});
+salesOffices.listen('squareMeter110', function(price) {
+    // 小红订阅 110 平方米房子的消息
+    console.log('价格= ' + price);
+    // 输出： 3000000
+});
+salesOffices.trigger('squareMeter88', 2000000);
+salesOffices.trigger('squareMeter110', 3000000);
