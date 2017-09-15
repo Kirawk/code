960cd1ba9141b4299b20a0c7c5fce22809e69862
Kirawk <1184355944@qq.com>
2017-09-15 23:18:45 +0800

    通用版观察订阅模式

--- Javascript设计模式/8.0.js
+++ Javascript设计模式/8.0.js
@@ -51,3 +51,40 @@ salesOffices.listen('squareMeter110', function(price) {
 });
 salesOffices.trigger('squareMeter88', 2000000);
 salesOffices.trigger('squareMeter110', 3000000);
+
+
+/*发布-订阅模式通用版*/
+var event ={
+    clientList:[],
+    listen: function(key,fn){
+        if(!this.clientList[key]){
+            this.clientList[key]=[];
+        }
+        this.clientList[key].push[fn];
+    },
+    trigger:function(){
+        var key=Array.prototype.shift.call(arguments),
+        fns = this.clientList[key];
+        if(!fns||fns.length===0){
+            return false;
+        }
+        for(var i=0,fn;fn=fns[i++];){
+            fn.apply(this,arguments);
+        }
+    }
+};
+var installEvent = function(obj){
+    for(var i in event){
+        obj[i]=event[i];
+    }
+};
+var salesOffices={};
+installEvent(salesOffices);
+salesOffices.listen( 'squareMeter88', function( price ){ // 小明订阅消息
+ console.log( '价格= ' + price );
+});
+salesOffices.listen( 'squareMeter100', function( price ){ // 小红订阅消息
+ console.log( '价格= ' + price );
+});
+salesOffices.trigger( 'squareMeter88', 2000000 ); // 输出：2000000
+salesOffices.trigger( 'squareMeter100', 3000000 ); // 输出：3000000 
