990f88093deb5b39a0928910baa223935e02a61e
Kirawk <1184355944@qq.com>
2017-09-27 22:39:45 +0800

    不使用模式解决绑定问题

--- Javascript设计模式/9.0.js
+++ Javascript设计模式/9.0.js
@@ -46,3 +46,25 @@ var delSubMenuCommand = new DelSubMenuCommand(SubMenu);
 setCommand(button1,refreshMenuBarCommand);
 setCommand(button2,addSubMenuCommand);
 setCommand(button2,delSubMenuCommand);
+
+/*不引入模式*/
+var bindClick = function(button,func){
+	button.onclick = func;
+};
+var MenuBar = {
+	refresh: function(){
+		console.log("刷新菜单页面");
+	}
+};
+
+var SubMenu = {
+	add:function(){
+		console.log("增加子菜单");
+	},
+	del:function(){
+		console.log("删除子菜单");
+	}
+};
+bindClick(button1,MenuBar.refresh);
+bindClick(button2,SubMenu.add);
+bindClick(button3,SubMenu.del);
