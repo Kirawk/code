0cc403e4db6d32c7f5b59f2aa882627365c18b82
Kirawk <1184355944@qq.com>
2017-10-08 22:56:58 +0800

    定义宏命令 MacroCommand

--- Javascript设计模式/9.0.js
+++ Javascript设计模式/9.0.js
@@ -228,3 +228,23 @@ var openQQCommand ={
         console.log("登录QQ");
     }
 };
+
+var MacroCommand = function(){
+    return{
+        commandsList:[],
+        add:function(command){
+            this.commandsList.push(command);
+        },
+        execute:function(){
+            for(var i=0,command;command=this.commandsList[i++];){
+
+                command.execute();
+            }
+        }
+    }
+};
+var macroCommand = MacroCommand();
+macroCommand.add( closeDoorCommand );
+macroCommand.add( openPcCommand );
+macroCommand.add( openQQCommand );
+macroCommand.execute(); 
