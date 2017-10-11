4fce04765bb84666708528c532b42c151fb953a3
Kirawk <1184355944@qq.com>
2017-10-11 23:14:38 +0800

    组合模式初试

---
+++ Javascript设计模式/10.js
@@ -0,0 +1,17 @@
+/**************组合模式******************/
+var closeDoorCommand = {
+	execute:function(){
+		console.log('关门');
+	}
+};
+var openPcCommadn = {
+    execute:function(){
+        console.log("开电脑");
+    }
+};
+
+var openQQCommand = {
+   execute:function(){
+       console.log("登录QQ")：
+   }
+}；
