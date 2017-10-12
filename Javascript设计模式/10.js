98ae58228d9603b055418f3237812ef55d5651ba
Kirawk <1184355944@qq.com>
2017-10-12 23:03:54 +0800

    宏命令

--- Javascript设计模式/10.js
+++ Javascript设计模式/10.js
@@ -12,6 +12,24 @@ var openPcCommadn = {
 
 var openQQCommand = {
    execute:function(){
-       console.log("登录QQ")：
+       console.log("登录QQ");
    }
-}；
+};
+var MacroCommand = function() {
+    return {
+        commandsList: [],
+        add: function(command) {
+            this.commandsList.push(command);
+        },
+        execute: function() {
+            for (var i = 0, command; command = this.commandsList[i++]; ) {
+                command.execute();
+            }
+        }
+    }
+};
+var macroCommand = MacroCommand();
+macroCommand.add(closeDoorCommand);
+macroCommand.add(openPcCommand);
+macroCommand.add(openQQCommand);
+macroCommand.execute();
