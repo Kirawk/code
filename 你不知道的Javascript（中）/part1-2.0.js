78a5e5f97166fb97079b562d9fcc57dbf9aab423
Kirawk <1184355944@qq.com>
2017-10-19 22:50:49 +0800

    字符与字符数组的比较

--- 你不知道的Javascript（中）/part1-2.0.js
+++ 你不知道的Javascript（中）/part1-2.0.js
@@ -35,3 +35,49 @@ a.foobar;//2
     console.log(arr);
 }
 foo("bar","baz");// ["bar", "baz", "bam"]
+
+
+/**
+字符串
+*/
+var a = "foo";
+var b = ["f","o","o"];
+a.length;//3
+b.length;//3
+a.indexOf("o");//1;
+b.indexOf("o");//1
+
+var c = a.concat("bar");
+var d = b.concat(["b","a","r"]);
+
+a === c;//false
+b === d;//false
+
+a[1] = "0";
+b[1] ="0";
+a;//foo
+b;//["f", "0", "o"]
+
+c = a.toUpperCase();
+a === c;//false
+a;//foo
+c;//FOO
+
+b.push("!");
+b;// ["f", "0", "o", "!"]
+
+a.join;//undefined
+a.map;//undefined
+
+var c = Array.prototype.join.call(a,"-");
+ var d = Array.prototype.map.call(a,function(v){
+    return v.toUpperCase()+".";
+ }).join("");
+c;//f-o-o
+d;//F.O.O.
+
+a.reverse;//undefined
+b.reverse();//["!", "o", "0", "f"]
+
+var c =a.split("").reverse().join("");
+c; //oof
