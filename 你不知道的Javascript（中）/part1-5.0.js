cea8a86b6362f8c3817d61c44ede5f3551cb902d
kirawk <1184355944@qq.com>
2017-11-16 21:45:10 +0800

    上下文规则

--- 你不知道的Javascript（中）/part1-5.0.js
+++ 你不知道的Javascript（中）/part1-5.0.js
@@ -78,8 +78,96 @@ function vowls (str) {
 vowls("Hello world!");
 
 ／*5.1.3上下问规则*／
+/*上下文规则*/
+var a = {
+    foo:bar()
+};
 
 
+foo:for(var i=0; i<4; i++){
+    for(var j=0;j<4;j++){
+        //如果j和i相等，继续外层的循环
+        if(j==i){
+            continue foo;
+        }
+        //跳过奇数结果
+        if((j*i)%2 == 1){
+            continue;
+        }
+
+        console.log(i,j);
+    }
+}
+//1 0
+//2 0
+//2 1
+//3 0
+//3 2
+
+function foo(){
+    bar:{
+        console.log("hello");
+        break bar;
+        console.log("never runs");
+    }
+    console.log("world!");
+}
+foo();
+//hello
+//world
+
+//代码快
+[]+{};//[object Object]
+{}+[];//0
+
+//对象解构
+function getData(){
+    //
+    return {
+        a:42,
+        b:"foo"
+    };
+}
+var {a,b} = getData();
+console.log(a,b);//42 "foo";
+//类似下面的代码
+var res = getData();
+var a = res.a;
+var b = res.b;
+
+function foo({a,b,c}){
+    console.log(a,b,c);
+}
+foo({
+    c:[1,2,3],
+    a:42,
+    b:"foo"
+});
+
+//else if(javascript 没有if else if else)
+
+if(a){
+    //
+}else if(b){
+//
+}else{
+  //  
+}　
+//本质
+if(a)dosomenthing(a);
+if(a){
+    //
+}else{
+    if(b){
+        //..
+    }else{
+        //..
+    }
+}
+
+/**
+*@ 5.2 运算符优先级
+**／
 
 
 
