d5b9f638c7cac5ecaa881795078290c5969255fc
kirawk <1184355944@qq.com>
2017-12-02 23:51:16 +0800

    promis信任问题

--- 你不知道的Javascript（中）/part2-3.0.js
+++ 你不知道的Javascript（中）/part2-3.0.js
@@ -208,3 +208,6 @@ var p1 = new Promise(function(resolve,reject){
      resolve(42);
 });
 var p2 = Promise.resolve(42);
+var p1 = Promise.resolve(42);
+var p2 = Promise.resolve(p1);
+p1 === p2;//true
