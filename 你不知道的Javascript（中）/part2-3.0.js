0523840a134e2ffebc20fdaff46f6fe3b73efa95
kirawk <1184355944@qq.com>
2017-12-12 22:45:29 +0800

    map的使用

--- 你不知道的Javascript（中）/part2-3.0.js
+++ 你不知道的Javascript（中）/part2-3.0.js
@@ -585,5 +585,27 @@ if(!Promise.map){
     };
 }
 
+var p1 = Promise.resolve(21);
+var p2 = Promise.resolve(42);
+var p3 = Promise.reject("Oops");
+
+Promise.map([p1,p2,p3],function(pr,done){
+    Promise.resolve(pr)
+    .then(
+        function(v){
+            done(v*2);
+        },
+        done
+    );
+})
+.then(function(vals){
+    console.log(vals);
+});
+
+/**
+ * 3.7 Promise API概述
+ */
+
+
 
 
