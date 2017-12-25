def3ad5cd18a84ee9893fb35550e1cf470723f6b
kirawk <1184355944@qq.com>
2017-12-25 22:47:18 +0800

    递归委托

--- 你不知道的Javascript（中）/part2-4.0.js
+++ 你不知道的Javascript（中）/part2-4.0.js
@@ -659,5 +659,23 @@ function *bar(){
 }
 run(bar);
 
+/**
+ * 4.5.4 递归委托
+ */
+function *foo(){
+    if(val > 1){
+        //生成器递归
+        val = yield *foo(val-1);
+    }
+    return yield request("http://some.url/?v="+val);
+}
 
+function *bar(){
+    var r1 = yield *foo(3);
+    console.log(r1);
+}
+run(bar);
+/**
+ * 4.6 生成器并发
+ */
 
