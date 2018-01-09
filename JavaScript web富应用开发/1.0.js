8a6cc67387e3a9ec526e90a099a5ffebca1b09f2
kirawk <1184355944@qq.com>
2018-01-09 23:01:26 +0800

    基于原型类继承+类库的作用域

--- JavaScript web富应用开发/1.0.js
+++ JavaScript web富应用开发/1.0.js
@@ -142,3 +142,187 @@ Person.extended({
         console.log(klass,"was extended!");
     }
 });
+
+/**
+ *基于原型的类继承 
+ */
+var Animal = function(){};
+Animal.prototype.breath = function(){
+    console.log("breath");
+};
+var Dog = function(){};
+
+//Dog继承了Animal;
+Dog.prototype = new Animal;
+Dog.prototype.wag = function(){
+    console.log("wag tail");
+};
+
+var dog = new Dog;
+dog.wag();
+dog.breath();//继承的属性
+
+/**
+ * 给类库添加继承
+ */
+var Class = function(parent){
+    var klass = function(){
+        this.init.apply(this,arguments);
+    };
+    //改变klass的原型
+    if(parent){
+        var subclass = function(){};
+        subclass.prototype = parent.prototype;
+        klass.prototype = new subclass;
+    };
+    klass.prototype.init = function(){};
+
+    //定义别名
+    klass.fn = klass.prototype;
+    klass.fn.parent = klass;
+    klass._super = klass.__proto__;
+    //给类添加属性
+    klass.extend = function(obj){
+        var extended = obj.extended;
+        for(var i in obj){
+            klass[i] = obj[i];
+        }
+        if(extended) extended(klass);
+    };
+
+    //给实例添加属性
+    klass.include = function(obj){
+        var included = obj.included;
+        for(var i in obj){
+            klass.fn[i] = obj[i];
+        }
+        if(included) included(klass)
+    };
+    return klass;
+};
+
+var Animal = new Class;
+Animal.include({
+    breath:function(){
+        console.log("breath");
+    }
+});
+
+var Cat = new Class(Animal);
+
+var tommy = new Cat;
+tommy.breath();
+
+/**
+ * 函数调用
+ */
+//理解call与apply
+$('.clicky').click(function(){
+    //thisz指向当前节点
+    $(this).hide();
+});
+
+$('p').each(function(){
+    //this指向本次迭代
+    $(this).remove();
+});
+
+var clicky = {
+    wasClicked: function(){
+
+    },
+    addListeners: function(){
+        var self = this;
+        $('.clicky').click(function(){
+            self.wasClicked()
+        });
+    }
+};
+clicky.addListeners();
+
+var proxy = function(func,thisObject){
+    return(function(){
+        return func.apply(thisObject,arguments);
+    });
+
+};
+var clicky = {
+    wasClicked:function(){
+    },
+    addListeners:function(){
+        var self = this;
+        $('.clickky').click(proxy(this.wasClicked,this));
+    }
+};
+var App = {
+    log:function(){
+        if(typeof console == "undefined") return;
+        var args = jQuery.makeArray(arguments);
+        // 插入一个新的参数
+        args.unshift("(App)");
+
+        //委托给console
+        console.log.apply(console,args);
+    }
+};
+
+/**
+ * 控制类库的作用域
+ */
+var Class = function(parent){
+    var klass = function(){
+        this.init.apply(this,arguments);
+    };
+    klass.prototype.init = function(){};
+    klass.fn = klass.prototype;
+
+    //添加一个proxy函数
+    klass.proxy = function(func){
+        var self = this;
+        return (function(){
+            return func.apply(self,arguments);
+        });
+    }
+
+    //在实例中也添加这个函数
+    klass.fn.proxy = klass.proxy;
+    return klass;
+};
+var Button = new Class;
+Button.include({
+    init:function(element){
+        this.element = jQuery(element);
+
+        //代理了这个click函数
+        this.element.click(this.proxy(this.click));
+    },
+    click: function(){}
+});
+//使用bind绑定
+Button.include({
+    init:function(element){
+        this.element = jQuery(element);
+
+        //绑定这个click函数
+        this.element.click(this.click.bind(this));
+    },
+    click:function(){}
+});
+
+if(!Function.prototype.bind){
+    Function.prototype.bind = function(obj){
+        var slice = [].slice,
+        args = slice.call(arguments,1),
+        self = this,
+        nop = function(){},
+        bound = function(){
+            return self.apply(this instanceof nop?this:(obj||{}),args.concat(slice.call(arguments)));
+        };
+        nop.prototype = self.prototype;
+        bound.prototype = new nop();
+        return bound;
+    };
+}
+/**
+ * 添加私有函数
+ */
