f2acb8bdd8d22058dc98720f78b0415839878572
kirawk <1184355944@qq.com>
2018-01-15 23:07:46 +0800

    激活第一个选项卡

--- JavaScript web富应用开发/2.0.js
+++ JavaScript web富应用开发/2.0.js
@@ -55,5 +55,9 @@ jQuery.fn.tabs = function(control){
         control.find(">[data-tab]").removeClass("active");
         control.find(">[data-tab='"+tabName+"']").addClass("active");
     })
+    //激活第一个选项卡
+    element.find("li:first").addClass("active");
+    //返回this以启用链式调用
+    return this;
 }
 
