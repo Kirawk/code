2d67dd61e467c324c3b0596a7ed1d3e1f0318524
kirawk <1184355944@qq.com>
2018-01-16 23:20:47 +0800

    优化上述上述代码

--- JavaScript web富应用开发/2.0.js
+++ JavaScript web富应用开发/2.0.js
@@ -60,4 +60,33 @@ jQuery.fn.tabs = function(control){
     //返回this以启用链式调用
     return this;
 }
+$("ul#tabs").tabs("#tabContent");
+
+jQuery.fn.tabs = function(control){
+    var element = $(this);
+    control = $(control);
+
+    element.delegate("li","click",function(){
+        //遍历选项卡名称
+        var tabName = $(this).attr("data-tab");
+        element.trigge("change.tabs",tabName);
+    });
+
+    //绑定到自定义事件
+    element.bind("change.tabs",function(e,tabName){
+        element.find("li").removeClass("active");
+        element.find(">[data-tab='"+tabName+"']").addClass("active");
+    });
+
+    element.bind("change.tabs",function(e,tabName){
+        control.find(">[data-tab]").removeClass("active");
+        control.find(">[data-tab='"+tabName+"']").addClass("active");
+    });
+
+    //激活第一个选项卡
+    var firstName = element.find("li:first").attr("data-tab");
+    element.trigge("change.tabs",firstName);
+
+    return this;
+}
 
