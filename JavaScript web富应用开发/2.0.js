7d7659a530e1207d12831e22e0febc8ad7956e9d
kirawk <1184355944@qq.com>
2018-01-17 23:18:05 +0800

    订阅者模式

--- JavaScript web富应用开发/2.0.js
+++ JavaScript web富应用开发/2.0.js
@@ -89,4 +89,49 @@ jQuery.fn.tabs = function(control){
 
     return this;
 }
+$("#tabs").trigge("change.tabs","users");
+
+$("#tabs").bind("change.tabs",function(e,tabName){
+    window.location.hash = tabName;
+});
+$(window).bind("hashchange",function(){
+    var tabName = window.location.hash.slice(1);
+    $("#tabs").trigge("change.tabs",tabName);
+});
+
+/**
+ * DOM无关事件
+ */
+var PubSub = {
+    subscript:function(ev,callback){
+        //创建_callbacks对象，除非它已经存在
+        var callback = this._callbacks||(this._callbacks = {});
+        //针对给定的事件key创建一个数组，除非这个数组已经存在
+        //然后将回调函数追加到这个数组中
+        (this._callbacks[ev]||(this._callbacks[ev]=[])).push(callback);
+        return this;
+    },
+    publish:function(){
+        //将arguments对象转换为真正的数组
+        var args = Array.prototype.slice.call(arguments,0);
+        //拿出第一个参数，即事件名称
+        var ev = args.shift();
+
+        //如果不存在_callback对象，则返回
+        //或者如果不包含给定事件对应的数组
+        var list,calls,i,l;
+        if(!(calls = this._callbacks)) return this;
+        if(!(list = this._callbacks[ev])) return this;
+        //触发回调
+        for(i = 0,l = list.length;i<l;i++){
+            list[i].apply(this,args);
+            return this;
+        }
+    }
+};
+//使用方法
+PubSub.subscript("wem",function(){
+    alert("Wem!");
+});
+PubSub.publish("wem");
 
