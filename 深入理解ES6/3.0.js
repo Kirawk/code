343f20befa91d3ddae407058ceaa1df33d4378a5
kirawk <1184355944@qq.com>
2018-03-07 23:17:50 +0800

    箭头函数语法

---
+++ 深入理解ES6/3.0.js
@@ -0,0 +1,56 @@
+/*****箭头函数****/
+/**
+ * 箭头函数语法
+ */
+
+//一个参数
+let value = value => value;
+let value = function(value) {
+    return value;
+};
+
+//两个参数
+let sum = (num1, num2) => num1 + num2;
+let sum = function(num1, num2) {
+    return num1 + num2;
+};
+
+//没参数
+let getName = () => "Nicholas";
+let getName = function() {
+    return "Nicholas";
+};
+
+//多个参数与多个表达式
+let a = (x, y) => {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+};
+let a = function(x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+};
+
+//创建一个空函数
+let doNothing = () => {};
+let doNothing = function() {};
+
+//创建一个对象字面量
+let getTempItem = id = ({ id: id, name: "Temp" });
+//实际上相当于
+let getTempItem = function() {
+    return {
+        id: id,
+        name: "Temp"
+    };
+};
+
+/**
+ * 创建立即执行函数
+ */
