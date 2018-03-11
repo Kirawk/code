4f70ab25954e94f1bc739ba181b00bb99a12cb89
kirawk <1184355944@qq.com>
2018-03-11 23:05:48 +0800

    模拟实现set与map集合

---
+++ 深入理解ES6/7.0.js
@@ -0,0 +1,38 @@
+/**
+ * Set集合和Map集合
+ */
+
+/**
+ * ECMAScript5中的Set集合和Map集合
+ */
+var set = Object.create(null);
+set.foo = true;
+
+//检查属性是否存在
+if (set.foo) {
+    //要执行的代码
+}
+
+var map = Object.create(null);
+map.foo = "bar";
+var value = map.foo;
+console.log(value);
+
+/**
+ * 该方案存在的问题
+ */
+var map = Object.create(null);
+map[5] = "bar";
+console.log(map["5"]); //"bar"
+
+var map = Object.create(null),
+    key1 = {},
+    key2 = {};
+map[key1] = "foo";
+console.log(map[key2]); //"foo"
+
+var map = Object.create(null);
+map.count = 1;
+if (map.count) {
+    //要执行的代码
+}
