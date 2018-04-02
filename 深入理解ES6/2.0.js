1177571478bfdca8c158fc7a83bed1b05990a139
kirawk <1184355944@qq.com>
2018-04-02 23:19:21 +0800

    了解UTF-16码位

--- 深入理解ES6/2.0.js
+++ 深入理解ES6/2.0.js
@@ -2,8 +2,10 @@
  * 字符串与正则表达式
  */
 
-var text = "";
+var text = "吉";
 console.log(text.length);
 console.log(/^.$/.test(text));
 console.log(text.charAt(0));
 console.log(text.charAt(1));
+console.log(text.charCodeAt(0));
+console.log(text.charCodeAt(1)); //57271
