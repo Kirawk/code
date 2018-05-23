50296930017c471df6418c691cee16347af92394
kirawk <1184355944@qq.com>
2018-05-23 23:08:26 +0800

    解构赋值

--- 深入理解ES6/5.0.js
+++ 深入理解ES6/5.0.js
@@ -18,3 +18,15 @@ let node = {
 let { type, name } = node;
 console.log(type); //"Identifier"
 console.log(name); //"foo"
+
+//解构赋值
+let node = {
+        type: 'Identifier',
+        name: 'foo'
+    },
+    type = "Literal",
+    name = 5;
+//使用解构语法为多个变量赋值
+({ type, name } = node);
+console.log(type); //"Identifier"
+console.log(name); //"foo"
