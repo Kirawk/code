daba55402a6eaef2d473fbb60d94e1061c7a51b2
kirawk <1184355944@qq.com>
2018-06-11 23:29:03 +0800

    迭代器初试

---
+++ 深入理解ES6/8.0.js
@@ -0,0 +1,22 @@
+/**
+ * 迭代器与生成器
+ */
+var colors = ["red", "green", "blue"];
+for (var i = 0, len = colors.length; i < len; i++) {
+    console.log(colors[i]);
+}
+
+function createIterator(items) {
+    var i = 0;
+    return {
+        next: function() {
+            var done = (i >= items.length);
+            var value = !done ? items[i++] : undefined;
+            return {
+                done: done,
+                value: value
+            };
+        }
+    };
+}
+var iterator = createIterator([1, 2, 3]);
