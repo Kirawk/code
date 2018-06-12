c1ca50d6d8fe8018c6b4274279aa4d457d88d4a8
kirawk <1184355944@qq.com>
2018-06-12 22:38:41 +0800

    生成器的三种表现形式

--- 深入理解ES6/8.0.js
+++ 深入理解ES6/8.0.js
@@ -20,3 +20,42 @@ function createIterator(items) {
     };
 }
 var iterator = createIterator([1, 2, 3]);
+
+//生成器
+function* createIterator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+let iterator = createIterator();
+console.log(iterator.next().value); //1
+console.log(iterator.next().value); //2
+console.log(iterator.next().value); //3
+
+function* createIterator(items) {
+    for (let i = 0; i < items.length; i++) {
+        yield items[i];
+    }
+}
+
+let iterator = createIterator([1, 2, 3]);
+console.log(iterator.next()); //1
+console.log(iterator.next()); //2
+console.log(iterator.next()); //3
+console.log(iterator.next()); //{value:undefined done:true}
+
+//生成器函数表达式
+let iterator = function* createIterator(items) {
+    for (let i = 0; i < items.length; i++) {
+        yield items[i];
+    }
+}
+
+//生成器对象的方法
+let o = {
+    createIterator: function*(items) {
+        for (let i = 0; i < items.length; i++) {
+            yield items[i];
+        }
+    }
+}
