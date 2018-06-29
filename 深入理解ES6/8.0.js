307ec488c840e4a3a162f91ec47f0953ad645baa
kirawk <1184355944@qq.com>
2018-06-29 23:36:22 +0800

    迭代器中抛出错误

--- 深入理解ES6/8.0.js
+++ 深入理解ES6/8.0.js
@@ -211,3 +211,14 @@ console.log(iterator.next());
 console.log(iterator.next(4));
 console.log(iterator.next(5));
 console.log(iterator.next());
+
+function* createIterator() {
+    let first = yield 1;
+    let second = yield first + 2;
+    yield second + 3;
+}
+let iterator = createIterator();
+console.log(iterator.next());
+console.log(iterator.next(4));
+console.log(iterator.throw(new Error("boom")));
+console.log(iterator.next());
