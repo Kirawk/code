dde0135d13e5fc154aa0863bb3a08f24b41de19a
kirawk <1184355944@qq.com>
2018-07-23 23:19:34 +0800

    可计算成员名称

--- 深入理解ES6/9.0.js
+++ 深入理解ES6/9.0.js
@@ -176,3 +176,16 @@ class PersonClass {
 };
 let me = new PeronClass("Nicholas");
 me.sayName();
+
+let propertyName = "html";
+class CustomHTMLElement {
+    constructor(element) {
+        this.element = element;
+    }
+    get[propertyName]() {
+        return this.element.innerHTML;
+    }
+    set[propertyName](value) {
+        this.element.innerHTML = value;
+    }
+}
